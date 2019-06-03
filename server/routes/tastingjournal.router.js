f8d5f57941c6434f969b05a510311cca51714e65
Austin Burgard <awburgard@gmail.com>
2019-06-03 16:20:19 -0500

    post route for user and tasting journal created and working

--- server/routes/tastingjournal.router.js
+++ server/routes/tastingjournal.router.js
@@ -15,4 +15,25 @@ router.get('/', (req, res) => {
         });
 });
 
+router.post('/', (req, res) => {
+    const queryString = `INSERT INTO "tasting_journal" ("coffee_name", "coffee_shop_id", "user_id", "description", "overall",
+                                                        "date", "aroma", "flavor", "aftertaste", "acidity", "sweetness",
+                                                        "mouthfeel", "balance", "clean_cup", "uniformity")
+                        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15)`;
+
+    console.log(req.body.coffee_shop_id);
+
+    pool.query(queryString, [req.body.coffee_name, req.body.coffee_shop_id, req.body.user_id, req.body.description,
+                            req.body.overall, req.body.date, req.body.aroma, req.body.flavor, req.body.aftertaste,
+                            req.body.acidity, req.body.sweetness, req.body.mouthfeel, req.body.balance,
+                            req.body.clean_cup, req.body.uniformity])
+        .then((response) => {
+            res.sendStatus(201);
+        })
+        .catch((err) => {
+            console.log(`Error posting tags to users table: ${err}`);
+            res.sendStatus(500);
+        });
+})
+
 module.exports = router;
--- server/routes/users.router.js
+++ server/routes/users.router.js
@@ -15,4 +15,17 @@ router.get('/', (req, res) => {
         });
 });
 
+router.post('/', (req,res) => {
+    const queryString = `INSERT INTO "users" ("username", "password", "first_name", "last_name")
+                        VALUES ($1, $2, $3, $4)`;
+
+    pool.query(queryString, [req.body.username, req.body.password, req.body.first_name, req.body.last_name ])
+        .then((response) => {
+            res.sendStatus(201);
+        })
+        .catch((err) => {
+            console.log(`Error posting tags to users table: ${err}`);
+            res.sendStatus(500);
+        });
+})
 module.exports = router;
