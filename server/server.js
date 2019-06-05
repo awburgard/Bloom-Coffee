7ea0c92c33a7a5b84e9c998a4537fd4648aa297d
Austin Burgard <awburgard@gmail.com>
2019-06-05 10:21:04 -0500

    adds passport, dotenv, and sets up server-side middleware

--- server/server.js
+++ server/server.js
@@ -1,6 +1,10 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const sessionMiddleware = require('./modules/session-middleware');
+const passport = require('./strategies/user.strategy');
+
 const port = process.env.PORT || 5000;
 
 /** ---------- ROUTERS ---------- **/
@@ -10,9 +14,19 @@ const cities = require('./routes/cities.router');
 const coffeeShop = require('./routes/coffeeshop.router')
 
 /** ---------- MIDDLEWARE ---------- **/
+// Body parser middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+// Serve static files
 app.use(express.static('build'));
+
+// Passport Session Configuration //
+app.use(sessionMiddleware);
+
+// start up passport sessions
+app.use(passport.initialize());
+app.use(passport.session());
 
 /** ---------- ROUTES ---------- **/
 app.use('/api/users', users);
