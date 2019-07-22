ca43614723616c115c4191d4fe89ebc93059f63d
Austin Burgard <awburgard@gmail.com>
2019-07-22 15:39:40 -0500

    removes componentDidMount

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -31,11 +31,6 @@ const theme = createMuiTheme({
 
 
 class App extends Component {
-  componentDidMount() {
-    // this.props.dispatch({ type: 'FETCH_USER' });
-    this.props.dispatch({ type: 'GET_KC' });
-  }
-
   render() {
     return (
       <MuiThemeProvider theme={theme}>
@@ -43,55 +38,55 @@ class App extends Component {
           <CssBaseline>
             <Router>
               <ScrollToTop>
-              <ElevateAppBar />
-              <Switch>
-                {/* Visiting localhost:3000 will redirect to localhost:3000/home */}
-                <Redirect exact from="/" to="/landing" />
+                <ElevateAppBar />
+                <Switch>
+                  {/* Visiting localhost:3000 will redirect to localhost:3000/home */}
+                  <Redirect exact from="/" to="/landing" />
 
-                <ProtectedRoute
-                  exact
-                  path="/home"
-                  component={HomePage}
-                />
+                  <ProtectedRoute
+                    exact
+                    path="/home"
+                    component={HomePage}
+                  />
 
-                <Route
-                  exact
-                  path="/landing"
-                  component={LandingPage}
-                />
-                {/* Visiting localhost:3000/about will show the about page.
+                  <Route
+                    exact
+                    path="/landing"
+                    component={LandingPage}
+                  />
+                  {/* Visiting localhost:3000/about will show the about page.
             This is a route anyone can see, no login necessary */}
-                <Route
-                  exact
-                  path="/kansas-city"
-                  component={HomePage}
-                />
-                {/* For protected routes, the view could show one of several things on the same route.
+                  <Route
+                    exact
+                    path="/kansas-city"
+                    component={HomePage}
+                  />
+                  {/* For protected routes, the view could show one of several things on the same route.
             Visiting localhost:3000/home will show the UserPage if the user is logged in.
             If the user is not logged in, the ProtectedRoute will show the 'Login' or 'Register' page.
             Even though it seems like they are different pages, the user is always on localhost:3000/home */}
-                {/* This works the same as the other protected route, except that if the user is logged in,
+                  {/* This works the same as the other protected route, except that if the user is logged in,
             they will see the info page instead. */}
-                <ProtectedRoute
-                  exact
-                  path="/tasting-journal-main"
-                  component={TastingJournalMain}
-                />
+                  <ProtectedRoute
+                    exact
+                    path="/tasting-journal-main"
+                    component={TastingJournalMain}
+                  />
 
-                <ProtectedRoute
-                  exact
-                  path="/tasting-journal-entry-form"
-                  component={AddEntryForm}
-                />
+                  <ProtectedRoute
+                    exact
+                    path="/tasting-journal-entry-form"
+                    component={AddEntryForm}
+                  />
 
-                <ProtectedRoute
-                  exact
-                  path="/tasting-journal-edit-form"
-                  component={TastingJournalEditPage}
-                />
-                {/* If none of the other routes matched, we will show a 404. */}
-                <Route render={() => <h1>404</h1>} />
-              </Switch>
+                  <ProtectedRoute
+                    exact
+                    path="/tasting-journal-edit-form"
+                    component={TastingJournalEditPage}
+                  />
+                  {/* If none of the other routes matched, we will show a 404. */}
+                  <Route render={() => <h1>404</h1>} />
+                </Switch>
               </ScrollToTop>
             </Router>
           </CssBaseline>
